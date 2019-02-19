04dd8162dc40e579c417346677b75ed1d50ded30
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2019-02-19 14:22:49 +0800

    Add files via upload

---
+++ 句子中数出单词个数.cpp
@@ -0,0 +1,86 @@
+/*
+输入一个英文句子，单词间由若干空格隔开，数出有多少个单词，区分大小写 
+*/
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+int main(){
+	char ch[100];
+	cout << " 请输入一个英文句子：" << endl; 
+	gets(ch);
+	char temp[20][15] = {0};          //存放单词 
+	int count;
+	if(ch[0] == ' '){
+		count = 0;
+	}else{
+		count = 1;
+	} 
+	int i = 0;
+	int m = 0, n = 0;
+	bool bl = true;
+	while (ch[i] != '\0'){               //取单词 
+		if(ch[i] == ' ' && ch[i + 1] != ' ' &&  ch[i + 1] != '\0'){ 
+			count ++;
+			m++;
+			n = 0;
+		} else{
+			if(ch[i] != ' '){
+				temp [m][n++] = ch[i];
+			}
+			
+		}
+		i++;
+	} 
+	n = 14;
+	for (int i = 0;i <= m;i++){             //输出每个单词 
+		for (int j = 0; j <= n;j++){
+			cout << temp[i][j];
+		}
+		cout << endl;
+	} 
+	
+	 cout << "m:" << m << "n:" << n << endl;
+	 cout << " 单词数：" << count << endl << endl; 
+	 
+	for (int i = 0;i < m;i++){                    //寻找相同元素 
+		for(int j = i + 1; j <= m;j++){ 
+			for (int k = 0;k <= n;k++){
+				cout << "if前：" << "temp[" << i << "][" << k << "]："<<temp[i][k] 
+				<< "   and   " << "temp[" << j << "][" << k << "]：" << temp[j][k] << endl;
+					
+				if (temp[i][k] != temp[j][k] ){
+					bl = false;					
+					cout << temp[i][k] << "   and   " << temp[j][k] << endl;
+					cout << " 第一次bl:"   << bl << endl; 
+					break; 
+				}
+								
+			}
+			if (bl == true){
+				cout << " 执行bl == true" << endl;
+				count --;
+				for (int f = j; f < m;f++){     //删除一个相同元素 
+					for ( int g = 0;g < n;g++){
+					temp[f][g] = temp[f + 1][g];	
+					}				
+				}
+				m--;
+				j--;
+				cout << " m的中间值：" << m << endl;
+				cout << " 中间单词列表：" << endl;
+				for (int i = 0;i <= m;i++){             //输出每个单词 
+						for (int j = 0; j <= n;j++){
+							cout << temp[i][j];
+						}
+						cout << endl;
+				} 					
+				}
+			bl = true;
+			cout << " 第" << i << " 次：单词数：" << count << endl; 
+		}			
+	}
+	cout << " 共有" << count << " 个单词" << endl; 
+	cout << "m的最终值： "  << m << endl; 
+}
+
